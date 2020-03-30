d1cb6145d27da6e820d6f31312e9a6492ab8ca96
Talon Durrant <talonmdurrant@gmail.com>
2020-03-30 01:48:58 -0600

    added some placeholder images and worked on the card aspect of the sections

--- main.js
+++ main.js
@@ -1,4 +1,12 @@
-let card = document.querySelector('.card');
-card.addEventListener( 'click', function() {
+let toggleButtonUniversity = document.querySelector('.toggle-button-university');
+let toggleButtonProfessional = document.querySelector('.toggle-button-professional');
+
+toggleButtonUniversity.addEventListener( 'click', function() {
+  let card = document.querySelector('.card');
+  card.classList.toggle('is-flipped');
+})
+
+toggleButtonProfessional.addEventListener( 'click', function() {
+  let card = document.querySelector('.card');
   card.classList.toggle('is-flipped');
 })
